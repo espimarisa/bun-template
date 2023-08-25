4006a83a915bb9bd6370409f4c3f475aa1102ef2
Espi Marisa <contact@espi.me>
2023-08-25 02:51:22 -0500

    read if cute

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("read if cute");
+// Read if cute
+function readIfCute(cute = true) {
+  if (cute) {
+    console.log("Read if cute");
+  } else {
+    console.log("Read if cute");
+  }
+
+  console.log("Read if double cute");
+}
+
+readIfCute();
